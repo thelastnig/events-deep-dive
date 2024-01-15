08436c7c80a665662b671f5070ccc0873ab49a90
thelastnig <thelastnig@gmail.com>
2024-01-15 11:20:41 +0900

    added comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
-
+// test
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
